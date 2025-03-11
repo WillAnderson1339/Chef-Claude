f48610cee9292ca0529149fe55d5dd4a32204199
Will Anderson <WillAnderson1339@gmail.com>
2025-03-11 15:22:57 -0700

    removed key values from api-keys.js

--- frontend/src/services/api-keys.js
+++ frontend/src/services/api-keys.js
@@ -1,4 +1,2 @@
 
-export const ANTHROPIC_KEY = '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'
-export const HF_APIKEY = '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'
 
